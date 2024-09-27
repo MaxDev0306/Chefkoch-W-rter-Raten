ba258c95cd46b5e2e0c1b4992553a4fdf40a8198
Maxim Saweljew <129965521+MaxDev0306@users.noreply.github.com>
2024-09-27 16:26:08 +0000

    stuff

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,28 @@ const VoulRegex = RegExp('/[AEIOU]/gm');
 let roundsPlayed = 0;
 let PointTotal = 0;
 let guesThisRound = 0;
+const strategies: Map<string, number> = new Map([
+  ['default', 0],
+  ['first_vouls', 0],
+  ['k_when_eit', 0],
+  ['h_when_eit', 0],
+  ['g_when_un', 0],
+  ['c_when_li', 0],
+  ['q_when_ue_ua_uo_ui', 0],
+  ['l_when_i_and_g_c_h', 0],
+  ['v_when_er_or', 0],
+  ['add_voul_when_ck_or_ch', 0],
+  ['k_when_c', 0],
+  ['h_when_c', 0],
+  ['h_when_lic', 0],
+  ['i_when_l_and_g', 0],
+  ['g_when_li', 0],
+  ['t_when_hei_or_kei', 0],
+  ['voul_when_double_kon', 0],
+  ['error', 0]
+])
+let currentStrategie = '';
+let lastScore = 0;
 
 socket.on('connect', () => {
   // dein Bot ist verbunden
@@ -45,6 +67,15 @@ function init(data: InitData) {
       console.log('Rounds Played: ', roundsPlayed)
       console.log('Average Score: ', PointTotal / roundsPlayed)
     }
+
+    if (roundsPlayed % 30 === 0) {
+      const sorted = new Map([...strategies.entries()].sort((a, b) => b[1] - a[1]))
+      console.log('---------------------------------')
+      const keys = Array.from(sorted.keys())
+      keys.forEach((key) => {
+        console.log(key, sorted.get(key))
+      })
+    }
 }
 
 function result(data: ResultData) {
@@ -58,6 +89,12 @@ function result(data: ResultData) {
 }
 
 function round(data: RoundData, callback: (guess: string) => void) {
+  const lastStrategyScore = strategies.get(currentStrategie) ?? 0;
+  if (data.players[0].score === lastScore) {
+    strategies.set(currentStrategie, lastStrategyScore + 1);
+  }
+  
+    lastScore = data.players[0].score;
     const gues = chooseLetter(data).toUpperCase();
 
     guesThisRound ++;
@@ -84,87 +121,102 @@ function getFirstVoul(gues: string, data: RoundData): string {
 }
 
 function chooseLetter(data: RoundData) {
+  currentStrategie = 'default'
   const gues = guesChoices[0];
+  if (!gues) {
+    console.log('AHHH', guesChoices, data.guessed.length)
+    currentStrategie = 'error'
+    let intersection = chars.filter(x => data.guessed.includes(x));
+    return intersection[0]
+  }
 
-  if (guesThisRound < 3) {
+  if (guesThisRound < 2) {
+    currentStrategie = 'first_vouls'
     return getFirstVoul(gues, data);
   }
 
-    // ck/ch erkennen
-    if (data.word.includes('CH')) {
-      return getFirstVoul(gues ,data)
+  if (data.word.includes('CH') || data.word.includes('CK')) {
+    currentStrategie = 'add_voul_when_ck_or_ch'
+    return getFirstVoul(gues ,data)
+  }
+
+  if (data.word.includes('UN') && !data.guessed.includes('G')) {
+    currentStrategie = 'g_when_un'
+    return 'G'
+  }
+
+  if (data.word.includes('EIT')) {
+    if (!data.guessed.includes('K')) {
+      currentStrategie = 'k_when_eit'
+      return 'K'
     }
-
-    if (data.word.includes('C')) {
-      if (!data.word.includes('CH') && !data.guessed.includes('K')){
-        return 'K'
-      }
-      if (!data.word.includes('CK') && !data.guessed.includes('H')){
-        return 'H'
-      }
-    }
-
-
-    // *ung erkennen
-    if (data.word.includes('UN') && !data.guessed.includes('N')) {
-      return 'N'
-    }
-
-    if (data.word.includes('U') && data.word.includes('N') && !data.guessed.includes('G')) {
-      return 'G'
-    }
-
-
-    // *lig/lich erkennen
-
-    if (!data.guessed.includes('L') && data.word.includes('I') && (data.word.includes('G') || data.word.includes('C') || data.word.includes('H'))) {
-      return 'L';
-    }
-
-    if (data.word.includes('LI') && !data.guessed.includes('C')) {
-      return 'C'
-    }
-
-    if (data.word.includes('LIC') && !data.guessed.includes('H')) {
+    
+    if (!data.guessed.includes('H')) {
+      currentStrategie = 'h_when_eit'
       return 'H'
     }
+  }
 
-    if (data.word.includes('L') && !data.guessed.includes('I') && data.word.includes('G')) {
-      return 'I';
+  if (data.word.includes('LI') && !data.guessed.includes('C')) {
+    currentStrategie = 'c_when_li'
+    return 'C'
+  }
+
+  if ((data.word.includes('UA') || data.word.includes('UE') || data.word.includes('UI') || data.word.includes('UO')) && !data.guessed.includes('Q')) {
+    currentStrategie = 'q_when_ue_ua_uo_ui'
+    return 'Q'
+  }
+
+  if (!data.guessed.includes('L') && data.word.includes('I') && (data.word.includes('G') || data.word.includes('C') || data.word.includes('H'))) {
+    currentStrategie = 'l_when_i_and_g_c_h'
+    return 'L';
+  }
+
+  if ((data.word.includes('ER') || data.word.includes('ER')) && !data.guessed.includes('V')) {
+    currentStrategie = 'v_when_er_or'
+    return 'V'
+  }
+
+
+  // NOT SORTED
+
+  if (data.word.includes('C')) {
+    if (!data.word.includes('CH') && !data.guessed.includes('K')){
+      currentStrategie = 'k_when_c'
+      return 'K'
     }
-
-    if (data.word.includes('LI') && !data.guessed.includes('G')) {
-      return 'G';
+    if (!data.word.includes('CK') && !data.guessed.includes('H')){
+      currentStrategie = 'h_when_c'
+      return 'H'
     }
+  }
 
-    // (h/k)eit
-    if (data.word.includes('EIT')) {
-      if (!data.guessed.includes('H')) {
-        return 'H'
-      }
+  if (data.word.includes('LIC') && !data.guessed.includes('H')) {
+    currentStrategie = 'h_when_lic'
+    return 'H'
+  }
 
-      if (!data.guessed.includes('K')) {
-        return 'K'
-      }
+  if (data.word.includes('L') && !data.guessed.includes('I') && data.word.includes('G')) {
+    currentStrategie = 'i_when_l_and_g'
+    return 'I';
+  }
+
+  if (data.word.includes('LI') && !data.guessed.includes('G')) {
+    currentStrategie = 'g_when_li'
+    return 'G';
+  }
+
+  if ((data.word.includes('HEI') || data.word.includes('KEI')) && !data.guessed.includes('T')) {
+    currentStrategie = 't_when_hei_or_kei'
+    return 'T'
+  }
+
+  guesChoices.forEach((letter) => {
+    if (data.word.includes(letter + letter)) {
+      currentStrategie = 'voul_when_double_kon'
+      return getFirstVoul(gues, data);
     }
+  })
 
-    if ((data.word.includes('HEI') || data.word.includes('KEI')) && !data.guessed.includes('T')) {
-      return 'T'
-    }
-
-    if((data.word.includes('UE') || data.word.includes('UA') || data.word.includes('UO') || data.word.includes('UI')) && !data.guessed.includes('Q')){
-      return 'Q'
-    }
-
-    if ((data.word.includes('ER') || data.word.includes('ER')) && !data.guessed.includes('V')) {
-      return 'V'
-    }
-
-    guesChoices.forEach((letter) => {
-      if (data.word.includes(letter + letter)) {
-        return getFirstVoul(gues, data);
-      }
-    })
-
-    return gues;
+  return gues;
 }
