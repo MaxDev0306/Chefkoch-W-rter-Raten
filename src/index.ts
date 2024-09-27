9b9b9ef7a928e0aabbbebc739628c6bf2eb841f7
MayesticZ <julian.stover@icloud.com>
2024-09-27 11:37:41 +0200

    added dot env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import {io} from 'socket.io-client';
+require('dotenv').config()
 const SECRET = process.env.BOT_SECRET;
 const socket = io('https://games.uhno.de', {
   transports: ['websocket']
 });
+
 socket.on('connect', () => {
   // dein Bot ist verbunden
   socket.emit('authenticate', SECRET, (success: boolean) => {
