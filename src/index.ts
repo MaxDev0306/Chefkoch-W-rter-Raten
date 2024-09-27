c74edf263c900bc84704dc367b32c3ba6132bea9
bbgithub04 <b.brink@venabo.de>
2024-09-27 11:51:29 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const SECRET = process.env.BOT_SECRET;
 import { InitData, ResultData, RoundData } from './types';
 const socket = io('https://games.uhno.de', {
   transports: ['websocket']
-});
+})
 socket.on('connect', () => {
   // dein Bot ist verbunden
   socket.emit('authenticate', SECRET, (success: boolean) => {
@@ -15,6 +15,25 @@ socket.on('disconnect', () => {
   console.log('disconected')
 });
 
+
+const alphabet: string[] = ["E","N","R","S","T","I","A","H","L","n","e","n","e","n","e","n",]
+const guessed: string[] = []
+const score: number = 0;
+
+
+socket.on('data', (data, callback:(guess:string)=> void) => {
+    switch (data.type) {
+        case 'INIT':
+            init(data);
+            return;
+        case 'RESULT':
+            result(data);
+            return;
+        case 'ROUND':
+            round(data, callback);
+    }
+});
+
 function init(data: InitData) {
     console.log(data)
 }
@@ -23,6 +42,7 @@ function result(data: ResultData) {
     console.log(data)
 }
 
-function round(data: RoundData) {
+function round(data: RoundData,callback:(guess:string)=> void) {
     console.log(data)
+    callback("e")
 }
