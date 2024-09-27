ddf816f11ed7c1f1d12250efde851fb2428f8598
dragonsmako <rafaelros2005@gmail.com>
2024-09-27 11:58:38 +0200

    Init

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,9 @@ const socket = io('https://games.uhno.de', {
   transports: ['websocket']
 });
 socket.on('connect', () => {
-  // dein Bot ist verbunden
   socket.emit('authenticate', SECRET, (success: boolean) => {
-    console.log('connected: ', success)
+    console.log('Bot connected: ', success)
   });
-});
-socket.on('disconnect', () => {
-  console.log('disconected')
 });
 
 function init(data: InitData) {
@@ -23,6 +19,40 @@ function result(data: ResultData) {
     console.log(data)
 }
 
-function round(data: RoundData) {
-    console.log(data)
+function getRandomInt(max:number) {
+    return Math.floor(Math.random() * max);
 }
+
+function round(data: RoundData, callback: (guess:string) => void) {
+    console.log(data)
+
+    const alphabet = 'BCFGHJKLMOPQUVWXYZ';
+    const firstGuesses = "ENISRATD";
+
+    let guess = firstGuesses[getRandomInt(firstGuesses.length - 1)];
+
+    if (data.guessed.includes(guess) && !data.guessed.every(guessedChar => firstGuesses.includes(guessedChar))) {
+        guess = firstGuesses[getRandomInt(firstGuesses.length - 1)];
+    } else if (data.guessed.includes(guess)) {
+        guess = alphabet[getRandomInt(alphabet.length - 1)];
+    }
+
+    callback(guess);
+}
+
+socket.on('data', (data, callback) => {
+    switch (data.type) {
+        case 'INIT':
+            init(data);
+            return;
+        case 'RESULT':
+            result(data);
+            return;
+        case 'ROUND':
+            round(data, callback);
+    }
+});
+
+socket.on('disconnect', () => {
+  console.log('Bot disconnected')
+});
